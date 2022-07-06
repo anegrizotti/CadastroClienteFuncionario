e21842304f6ae6be5bdac59f6addb384200f8d65
slackbot115 <viniccius.carvalho22@gmail.com>
2022-07-06 11:43:32 -0300

    Adicionado dominio

--- src/main/java/com/mycompany/atv/trabeng/dominio/Cliente.java
+++ src/main/java/com/mycompany/atv/trabeng/dominio/Cliente.java
@@ -1,20 +1,18 @@
 package com.mycompany.atv.trabeng.dominio;
 
-public class Funcionario {
+public class Cliente {
     public String nome;
     public String cpf;
     public String email;
     public String telefone;
-    public double salario;
 
-    public Funcionario() {
+    public Cliente() {
     }
-    
-    public Funcionario(String nome, String cpf, String email, String telefone, double salario) {
+
+    public Cliente(String nome, String cpf, String email, String telefone) {
         this.nome = nome;
         this.cpf = cpf;
         this.email = email;
         this.telefone = telefone;
-        this.salario = salario;
     }
 }
